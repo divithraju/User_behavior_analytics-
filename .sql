68b204e6c9d13d8ef26e52e8267f325baf556103
Software Engineer-Data Engineer <118492176+divithraju@users.noreply.github.com>
2025-04-07 21:44:13 +0530

    Create .sql

---
+++ .sql
@@ -0,0 +1,43 @@
+-- users table
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    signup_date DATE,
+    region VARCHAR(50),
+    device_type VARCHAR(50),
+    marketing_source VARCHAR(100)
+);
+
+-- products table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price NUMERIC(10, 2)
+);
+
+-- campaigns table
+CREATE TABLE campaigns (
+    campaign_id INT PRIMARY KEY,
+    campaign_name VARCHAR(100),
+    start_date DATE,
+    end_date DATE,
+    budget INT
+);
+
+-- sessions table
+CREATE TABLE sessions (
+    session_id VARCHAR(20) PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    session_start TIMESTAMP,
+    session_end TIMESTAMP
+);
+
+-- events table
+CREATE TABLE events (
+    event_id VARCHAR(20) PRIMARY KEY,
+    session_id VARCHAR(20) REFERENCES sessions(session_id),
+    user_id INT REFERENCES users(user_id),
+    event_type VARCHAR(50),
+    product_id INT REFERENCES products(product_id),
+    event_time TIMESTAMP
+);
